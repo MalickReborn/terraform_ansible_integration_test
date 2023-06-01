9c6b660fee3f6b2cd24c82e5a3aefb32b1c836a1
MalickReborn <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>
2023-06-01 16:56:29 +0000

    first commit

---
+++ terraform/provider.tf
@@ -0,0 +1,14 @@
+
+provider "aws" {
+  version = "~> 4.0"
+  region  = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
